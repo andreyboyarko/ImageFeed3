e85914238992c43ef202a83526156ae37a1fcf3f
Andrei Boyarko <andreyboyarko@gmail.com>
2025-08-04 22:55:19 +0200

    пишем тесты

---
+++ Image FeedTests/WebViewTests.swift
@@ -0,0 +1,20 @@
+@testable import ImageFeed
+import XCTest
+
+final class WebViewTests: XCTestCase {
+
+    func testPresenterCallsLoadRequest() {
+        // given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        presenter.viewDidLoad()
+        
+        // then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+}
